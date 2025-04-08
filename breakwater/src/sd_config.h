60cec5bdeff451702ca993503879da112b2cff87
Ansh Vijay <anshvijay28@gmail.com>
2025-04-07 21:55:02 -0400

    changing params for 1000 clients

--- breakwater/src/sd_config.h
+++ breakwater/src/sd_config.h
@@ -50,8 +50,8 @@
 /* % error to trigger increase */
 #define SEDA_ERR_I			-0.5
 /* additive rate increase */
-#define SEDA_ADJ_I			4.0
+#define SEDA_ADJ_I			40
 /* multiplicative rate decrease */
-#define SEDA_ADJ_D			1.1
+#define SEDA_ADJ_D			1.04
 /* weight on additive increase */
 #define SEDA_CI				-0.1
