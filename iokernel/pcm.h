4618487bb26a9407d295862ee5fdfa48a64cf302
ppppaidy <ppppaidy@gmail.com>
2025-10-13 10:11:33 -0400

    extern get_total_channel_number

--- iokernel/pcm.h
+++ iokernel/pcm.h
@@ -2,6 +2,7 @@
 
 /* Declarations of relevant functions in patched PCM library */
 extern uint32_t pcm_caladan_get_cas_count(uint32_t channel);
+extern uint32_t pcm_caladan_get_channel_count(void);
 extern uint32_t pcm_caladan_get_active_channel_count(void);
 extern int pcm_caladan_init(int socket);
 
